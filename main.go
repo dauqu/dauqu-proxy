41c775e0fe2f8bc04d079d661a07787801603bc2
Harsh Singh <sujeetsingh734321@gmail.com>
2022-12-15 20:47:08 +0530

    Changes

--- main.go
+++ main.go
@@ -2,70 +2,88 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http/httputil"
 	"net/url"
 
-	"github.com/gin-gonic/autotls"
+	// "github.com/gin-gonic/autotls"
 	"github.com/gin-gonic/gin"
 )
 
-//Create JSON data
-var jsonData = []byte(`{
-	"domain": "d.setkaro.com",
-	"proxy": "localhost:5556",
-	"headers": ["X-Forwarded-For", "X-Forwarded-Proto", "X-Forwarded-Host", "X-Forwarded-Port"],
-	"sll": true
-}`)
-
+// Create JSON data
+var jsonData = []byte(`[
+	{
+		"domain": "localhost/1",
+		"proxy": "localhost:58441",
+		"headers": ["X-Forwarded-For", "X-Forwarded-Proto", "X-Forwarded-Host", "X-Forwarded-Port"],
+		"ssl": true
+	},
+	{
+		"domain": "localhost/2",
+		"proxy": "localhost:56710",
+		"headers": ["X-Forwarded-For", "X-Forwarded-Proto", "X-Forwarded-Host", "X-Forwarded-Port"],
+		"ssl": false
+	}
+]`)
 
 func main() {
 
 	//Create Details struct
 	type Details struct {
-		Domain string `json:"domain"`
-		Proxy  string `json:"proxy"`
+		Domain  string   `json:"domain"`
+		Proxy   string   `json:"proxy"`
 		Headers []string `json:"headers"`
-		SLL	bool `json:"sll"`
+		SSL     bool     `json:"sll"`
 	}
 
 	//Bind JSON to Details struct
-	var details Details
+	var details []Details
+
+	//Unmarshal JSON
 	err := json.Unmarshal(jsonData, &details)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	//Print details
+	fmt.Println(details)
 
-	//Create Gin router
-	router := gin.Default()
+	//Loop through details
+	for _, i := range details {
 
-	//Create proxy
-	proxy := httputil.NewSingleHostReverseProxy(&url.URL{
-		Scheme: "http",
-		Host:   details.Proxy,
-	})
+		//Create Gin router
+		router := gin.Default()
 
-	//Create proxy handler
-	proxyHandler := func(c *gin.Context) {
-		proxy.ServeHTTP(c.Writer, c.Request)
-	}
-
-	//Create proxy route
-	router.Any("/*path", proxyHandler)
-
-	//Add headers
-	for _, header := range details.Headers {
-		router.Use(func(c *gin.Context) {
-			c.Writer.Header().Set(header, c.Request.Header.Get(header))
-			c.Next()
+		//Create proxy
+		proxy := httputil.NewSingleHostReverseProxy(&url.URL{
+			Scheme: "http",
+			Host:   i.Proxy,
 		})
-	}
 
-	//Start server
-	if details.SLL {
-		autotls.Run(router, details.Domain)
-	} else {
+		//Create proxy handler
+		proxyHandler := func(c *gin.Context) {
+			proxy.ServeHTTP(c.Writer, c.Request)
+		}
+
+		//Create proxy route
+		router.Any("/*path", proxyHandler)
+
+		//Add headers
+		// for _, header := range details.Headers {
+		// 	router.Use(func(c *gin.Context) {
+		// 		c.Writer.Header().Set(header, c.Request.Header.Get(header))
+		// 		c.Next()
+		// 	})
+		// }
+
+		//Start server
+		// if i.SSL {
+		// 	autotls.Run(router, i.Domain)
+		// } else {
+		// 	router.Run(i.Domain)
+		// }
+
 		router.Run(":80")
 	}
 }
