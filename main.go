8785cdcb48e5da0b635c10c9e4faa0d5a0ac8915
Harsh Singh <sujeetsingh734321@gmail.com>
2022-12-16 19:18:32 +0530

    Changes

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	// "crypto/tls"
-	"crypto/tls"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -10,9 +8,6 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"os"
-
-	"golang.org/x/crypto/acme/autocert"
-	// "golang.org/x/crypto/acme/autocert"
 )
 
 type Domains struct {
@@ -39,19 +34,17 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// mux := http.NewServeMux()
-
 	//Loop through domains
 	for _, domain := range dauqu {
 
 		vhost, err := url.Parse(domain.Proxy)
 		if err != nil {
-			panic(err)
+			log.Fatal(err)
 		}
 
 		//Create proxy
 		proxy := httputil.NewSingleHostReverseProxy(&url.URL{
-			Scheme: "https",
+			Scheme: vhost.Scheme,
 			Host:   vhost.Host,
 		})
 
@@ -59,7 +52,6 @@ func main() {
 		// proxy.Director = func(req *http.Request) {
 		// 	req.Header.Set("X-Forwarded-Host", req.Header.Get("Host"))
 		// 	req.Header.Set("Host", vhost.Host)
-
 		// 	req.URL.Scheme = vhost.Scheme
 		// 	req.URL.Host = vhost.Host
 		// }
@@ -67,13 +59,9 @@ func main() {
 		//Header response
 		// proxy.ModifyResponse = func(resp *http.Response) error {
 		// 	resp.Header.Set("Server", "Setkaro")
-		// 	//SSL
 		// 	resp.Header.Set("Strict-Transport-Security", "max-age=63072000; includeSubDomains; preload")
-		// 	//HTTP2
 		// 	resp.Header.Set("Alt-Svc", "h2=\":443\"; ma=2592000")
-
 		// 	resp.Header.Set("X-Forwarded-Proto", "https")
-
 		// 	return nil
 		// }
 
@@ -82,21 +70,6 @@ func main() {
 		})
 	}
 
-	// Create autocert managers
-	m := autocert.Manager{
-		Prompt: autocert.AcceptTOS,
-		Cache:  autocert.DirCache("certs"),
-		Email:  "info@dauqu.com",
-	}
-
-	// Create server
-	server := &http.Server{
-		Addr:    ":443",
-		TLSConfig: &tls.Config{
-			GetCertificate: m.GetCertificate,
-		},
-	}
-
-	go http.ListenAndServe(":80", nil)
-	server.ListenAndServeTLS("", "")
+	//Listen and serve
+	http.ListenAndServe(":80", nil)
 }
